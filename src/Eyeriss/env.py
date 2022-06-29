fc0850ec57ea4220b2a4c26a69151787d3b1a9cf
Erwin <erwin@ErwindeMacBook-Pro.local>
2022-06-29 16:14:34 +0800

    senv

--- src/Eyeriss/env.py
+++ src/Eyeriss/env.py
@@ -4,8 +4,7 @@ import os
 from subprocess import Popen, PIPE
 import pandas as pd
 import math
-from multiprocessing.pool import Pool
-from multiprocessing import cpu_count
+
 m_type_dicts = {1:"CONV", 2:"DSCONV", 3:"CONV", 4:"TRCONV"}
 
 
@@ -58,28 +57,17 @@ class MaestroEnvironment(object):
         self.state = np.ones(self.slevel_max*(7+1), dtype=np.int32)
         self.state[0] = self.dim2id['K']
         self.state[self.level_steps] = self.dim2id['C']
-        # self.state = np.array([1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1])
         if self.slevel_min == 1:
             self.state = np.array([2, 1, 1, 1, 1, 1, 1, 1], dtype=np.int32)
         else:
             self.state = np.array([2, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1], dtype=np.int32)
-        # self.state[1] = min(self.dimension[self.state[0] - 1], self.num_pe)
-        # self.state[2] = min(self.dimension[self.state[0] - 1], self.num_pe)
-        # state: slevel_max*(para_dim, cluster_size, tile_size*6)
 
         self.mode = 0
-        # self.mode_sequence = [0, 2, 3, 4, 5, 6, 7]
         self.mode_sequence = [2, 3, 4, 5, 6, 7]
         random.shuffle(self.mode_sequence)
-        # for i in range(len(self.mode_sequence)):
-        #     if self.mode_sequence[i] == 0:
-        #         self.mode_sequence.insert(i+1, 1)
         self.mode_sequence.insert(0, 1)
         self.mode_sequence.insert(0, 0)
-        # print(self.mode_sequence)
-        # _, self.total_eps_reward, _ = self.get_reward()
         _, self.last_reward, _, _, _ = self.get_reward()
-        # print(self.last_reward)
         self.best_reward = float('-inf')
         self.min_reward = self.last_reward.copy()
         self.dimension = dimension
@@ -100,7 +88,6 @@ class MaestroEnvironment(object):
             self.last_level_tiles[1] = min(self.dimension[1], 64) // self.scale + 1
         self.last_level_tiles[2] = min(self.dimension[2], 56) // self.scale + 1
         self.last_level_tiles[3] = min(self.dimension[3], 56) // self.scale + 1
-        # self.last_level_tiles[3] = self.dimension[3] // 7 + 1
         for i in range(4, 6):
             self.last_level_tiles[i] = min(last_level_max_tiles[i], self.dimension[i])
         print(self.last_level_tiles)
@@ -122,56 +109,6 @@ class MaestroEnvironment(object):
         state_info['cur_levels'] = self.level
         return state_info
 
-    # def episode_reset(self):
-    #     self.level = self.slevel_min
-    #     self.mode = 0
-    #     # _, self.last_reward, _ = self.get_reward()
-    #
-    #     self.parallel_mask = np.array([0., 0., 0., 0.])
-    #     # self.last_level_tiles = np.array([10, 3, 11, 11, 7, 7])
-    #     self.last_level_tiles = np.array([10, 10, 15, 15, 7, 7])
-    #     if self.dimension[1] == 3:
-    #         self.last_level_tiles[1] = 2
-    #     for i in range(4, 6):
-    #         self.last_level_tiles[i] = min(self.last_level_tiles[i], self.dimension[i])
-    #     if self.dimension[2] == 224:
-    #         self.last_level_tiles[2] = 15
-    #         self.last_level_tiles[3] = 15
-    #     elif self.dimension[2] == 112:
-    #         self.last_level_tiles[2] = 13
-    #         self.last_level_tiles[3] = 13
-    #     elif self.dimension[2] == 56:
-    #         self.last_level_tiles[2] = 11
-    #         self.last_level_tiles[3] = 11
-    #     elif self.dimension[2] == 28:
-    #         self.last_level_tiles[2] = 9
-    #         self.last_level_tiles[3] = 9
-    #     elif self.dimension[2] == 14:
-    #         self.last_level_tiles[2] = 8
-    #         self.last_level_tiles[3] = 8
-    #     elif self.dimension[2] == 7:
-    #         self.last_level_tiles[2] = 7
-    #         self.last_level_tiles[3] = 7
-    #
-    #     next_state = np.zeros(self.slevel_max * self.level_steps)
-    #     next_state[0] = 1. / 16.
-    #     next_state[1] = self.state[0] / 4.
-    #     next_state[2:] = self.state[2:] / self.dim_max
-    #
-    #     state_info = {}
-    #     state_info['state'] = next_state
-    #     state_info['parallel_mask'] = self.parallel_mask
-    #     state_info['instruction'] = self.mode
-    #     state_info['last_level_tiles'] = self.last_level_tiles
-    #     return state_info
-
-    def scan_indv(self,indv):
-        last_cluster_dict=self.lastcluster_dict
-        for i in range(len(indv)-6,len(indv), 1):
-            d, d_sz = indv[i]
-            last_cluster_dict[d] = d_sz
-        return  last_cluster_dict
-
     def get_out_repr(self, x):
         if x in self.out_repr:
             return x
@@ -180,30 +117,17 @@ class MaestroEnvironment(object):
 
     def get_reward(self):
         sol = []
-        # for i in range(self.level):
         for i in range(self.total_levels):
             sol.append([self.id2dim[self.state[i*self.level_steps]], self.state[i*self.level_steps+1]])
             for j in range(1, 7):
                 sol.append([self.id2dim[j], self.state[i*self.level_steps+j+1]])
-
-        # print(self.state, sol)
         reward, latency, energy, l1_size, l2_size = self.oberserve_maestro(sol)
         constraint = (l1_size, l2_size)
-        # print(sol, reward, constraint)
-        # print(reward, l1_size, l2_size)
 
-        # if self.min_reward is None:
-        #     self.min_reward = reward
-        # reward_saved = reward.copy()
-        # self.min_reward = min(self.min_reward, reward_saved)
-        # reward -= self.min_reward
-        # self.total_eps_reward += reward
         return sol, reward, latency, energy, constraint
 
     def step(self, action):
-        # print (self.last_reward)
         state_info = {}
-        # print("step_start:", self.mode, self.mode_sequence[self.mode], action, self.state)
         done = 0
 
         if self.mode_sequence[self.mode] == 0:
@@ -246,12 +170,8 @@ class MaestroEnvironment(object):
                                                                  math.ceil(
                                                                      cluster_size / self.num_pe) // self.scale + 1)
                     cluster_size = min(cluster_size, self.num_pe)
-                    # self.last_level_tiles[0] = 1
-                    # self.last_level_tiles[1] = 1
                     if self.state[(self.level - 2) * 8 + 2] == 1:
                         self.parallel_mask[0] = float("-inf")
-                    # self.mode += 2
-                # print("cluster_size: ", cluster_size)
 
                 self.state[(self.level - 1) * self.level_steps + 1] = cluster_size
 
@@ -296,9 +216,6 @@ class MaestroEnvironment(object):
         l1_size, l2_size = constraint
         info = 'success'
         if reward is None:
-            # reward = -2.
-            # print("fail: ", self.min_reward, l1_size, l2_size)
-            # reward = self.min_reward * (l1_size / self.l1_size + l2_size / self.l2_size)
             reward = self.min_reward * 10
             info = "fail"
             done = 1
@@ -306,14 +223,9 @@ class MaestroEnvironment(object):
             self.last_reward = self.min_reward * 10
         else:
             reward_saved = reward.copy()
-            # if self.last_reward is None or reward_saved > self.last_reward:
-            #     reward = 2.
-            # else:
-            #     reward = -1.
             reward = reward_saved - self.last_reward
             self.last_reward = reward_saved
             self.min_reward = min(self.min_reward, reward_saved)
-            # print ("success:", reward)
 
         self.mode += 1
 
@@ -322,7 +234,6 @@ class MaestroEnvironment(object):
                 done = 1
             self.level += 1
             self.mode = 0
-        # print('end_step:', self.state, next_state, self.last_level_tiles, reward, constraint)
         state_info['state'] = next_state
         state_info['parallel_mask'] = self.parallel_mask
         state_info['instruction'] = self.mode_sequence[self.mode]
@@ -351,23 +262,11 @@ class MaestroEnvironment(object):
             fo.write("}\n")
             fo.write("}\n")
             fo.write("}")
-        # with open("{}.m".format(m_file), "r") as fo:
-        #     lines = fo.readlines()
-        #     for line in lines:
-        #         print(line)
 
     def oberserve_maestro(self, indv):
         m_file = "{}".format(random.randint(0, 2**32))
         self.write_maestro(indv,m_file=m_file)
-        # print(num_pe, bw, l1_size)
         os.remove("./{}.csv".format(m_file)) if os.path.exists("./{}.csv".format(m_file)) else None
-        # command = [self._executable,
-        #            "--Mapping_file={}.m".format(m_file),
-        #            "--full_buffer=false", "--noc_bw={}".format(self.NocBW),
-        #            "--noc_hops=1", "--noc_hop_latency=1",
-        #            "--noc_mc_support=true", "--num_pes={}".format(self.num_pe),
-        #            "--num_simd_lanes=1", "--l1_size={}".format(self.l1_size),
-        #            "--l2_size={}".format(self.l2_size), "--print_res=false", "--print_res_csv_file=true", "--print_log_file=false", "--print_design_space=false", "--msg_print_lv=0"]
         command = [self._executable,
                    "--Mapping_file={}.m".format(m_file),
                    "--full_buffer=false", "--noc_bw=81920000",
@@ -376,7 +275,6 @@ class MaestroEnvironment(object):
                    "--num_simd_lanes=1", "--l1_size=81920000",
                    "--l2_size=81920000", "--print_res=false", "--print_res_csv_file=true",
                    "--print_log_file=false", "--print_design_space=false", "--msg_print_lv=0"]
-
 
         process = Popen(command, stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
@@ -393,7 +291,6 @@ class MaestroEnvironment(object):
             l1_size = np.array(df[" L1 SRAM Size (Bytes)"]).reshape(-1, 1)
             l2_size = np.array(df["  L2 SRAM Size (Bytes)"]).reshape(-1, 1)
             mac = np.array(df[" Num MACs"]).reshape(-1, 1)
-            # print(runtime, throughput, energy, area, l1_size, l2_size, mac, power)
             os.remove("./{}.csv".format(m_file))  if os.path.exists("./{}.csv".format(m_file)) else None
             os.remove("./log.txt") if os.path.exists("./log.txt") else None
             self.observation = [np.mean(x) for x in [runtime, throughput, energy, area, l1_size, l2_size, mac, power]]
@@ -405,7 +302,6 @@ class MaestroEnvironment(object):
                     return False
 
             if len(str(stdout)) > 3 or catch_exception():
-                # print(stdout, catch_exception())
                 return None, None, None, np.mean(l1_size), np.mean(l2_size)
             return self.judge()
         except Exception as e:
@@ -443,16 +339,6 @@ class MaestroEnvironment(object):
             else:
                 raise NameError('Undefined fitness type')
             values.append(reward)
-            # print("values: ", values)
         values.append(l1_size)
         values.append(l2_size)
-        # return values
-        # return -energy * runtime, -runtime, -energy, l1_size, l2_size
         return reward, -runtime, -energy, l1_size, l2_size
-
-    def print_indv(self, indv,fd=False):
-        for k in range(0, len(indv), 7):
-            if fd:
-                fd.write("\n{}".format(indv[k:k+7]))
-            else:
-                print(indv[k:k+7])
